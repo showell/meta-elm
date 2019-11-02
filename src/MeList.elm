bd9f459483ae07623c40063deb2c766c30b5b25f
Steve Howell <showell30@yahoo.com>
2019-11-02 10:13:49 -0400

    Clean up MeList.sortBy

--- src/MeList.elm
+++ src/MeList.elm
@@ -144,66 +144,17 @@ sort unbox =
 
 sortByInt : Expr -> Expr
 sortByInt mapper =
-    sortBy MeInt.toInt 0 mapper
+    sortBy MeInt.toInt mapper
 
 
-sortBy : (V -> Result String comparable1) -> comparable1 -> Expr -> Expr
-sortBy unbox default ordExpr =
-    {--
-        This is a bit of work in progress.  There are a few
-        somewhat related problems:
-
-            - We don't report errors if any values are
-              the wrong type.  (If one value is wrong, all
-              values should be wrong, and our `ord` will just
-              return the same value each time, which means the
-              list will randomly sort.
-
-            - It's inefficient, as I do O(NlogN) unboxings.
-
-            - I have to pass in the hacky `default` thingy to
-              satisfy the compiler.
-
-        This is easy enough to fix--I should just unbox the list
-        first, which solves all the above problems.
-
-            - When unboxing, it's easy to return VError if
-              any bad element is encountered.  (Now I can't do
-              it, because List.sortBy is opaque to me.)
-
-            - I'll only do O(N) unbox/box operations. (2N total)
-
-            - I won't need the `default`.
-
-        The approach above will still use List.sortBy for the
-        heavy lifting (which is basically Elm.Kernel.List.sortBy).
-        It will require me to re-box everything in O(N) time.
-
-        SEE sort -- it's better, and we can probably have it
-                    accept an `ord` and rename it `sortHelper`
-                    or something to share code
-    --}
+sortBy : (V -> Result String comparable1) -> Expr -> Expr
+sortBy unbox ordExpr =
     let
-        happy_path : List Expr -> (Expr -> V) -> V
-        happy_path lst ord =
-            let
-                rawOrd item =
-                    case unbox (ord item) of
-                        Ok data ->
-                            data
-
-                        _ ->
-                            default
-            in
-            lst
-                |> List.sortBy rawOrd
-                |> VList
-
         f : FV
         f c expr =
             case ( computeV c expr, getFuncV c ordExpr ) of
                 ( VList lst, Ok ord ) ->
-                    happy_path lst (ord c)
+                    transformSort ord unbox c lst
 
                 ( _, Err s ) ->
                     VError ("bad mapper" ++ s)
